77ce1268ea16fa7e1a8a658a568e24cef05bd6ca
Saigeethab <saigeethab@BMDVSAIGEETHAB.local>
2026-02-26 15:38:27 +0530

    testing for invalid registration cases

---
+++ src/tests/authService.actions.spec.js
@@ -0,0 +1,53 @@
+import { describe, it, expect } from "vitest"
+import MockAdapter from "axios-mock-adapter"
+import axiosInstance from "../services/api.js"
+import { register } from "../services/authService.js"
+
+const mock = new MockAdapter(axiosInstance)
+describe("Auth service apis", () => {
+    it("Get User API", async () => {
+        const mockUser = {
+            "name": "Geetha",
+            "email": "bhumireddybsaigeetha@gmail.com",
+            "phone": "9014580108",
+            "password": "Reddygeetha@2004",
+        }
+        mock.onGet("/users").reply(200, mockUser)
+        const response = await axiosInstance.get("/users")
+        expect(response.data).toEqual(mockUser)
+    })
+
+    it("Post User API", async () => {
+        const mockUser = {
+            "name": "Geetha",
+            "email": "bhumireddybsaigeetha@gmail.com",
+            "phone": "9014580108",
+            "password": "Reddygeetha@2004",
+            "role": "waiter"
+        }
+        mock.onPost("/users").reply(201, mockUser)
+        const response = await axiosInstance.post("/users")
+        expect(response.data).toEqual(mockUser)
+    })
+
+
+
+})
+
+describe("invalid registration cases", ()=>{
+    it("all blank feilds return exception", async ()=>{
+        await expect(register({
+            email:'',
+            password:'',
+            phone:''
+        })).rejects.toThrow("All fields are required")
+    })
+
+    it("invalid email should return exception", async ()=>{
+        await expect(register({
+            email:'geetha@hhweurhw',
+            password:'dfdf',
+            phone:'9014580108'
+        })).rejects.toThrow("Please enter a valid email")
+    })
+})
